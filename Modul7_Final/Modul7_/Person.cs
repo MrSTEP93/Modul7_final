4af5ecc3d7fdcb526b09357d80820354868995d4
Shatalov.AG <mrstep93@ya.ru>
2024-01-17 20:49:51 +0300

    закинул файлы проекта

---
+++ Modul7_Final/Modul7_/Person.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazarMiniMarket
+{
+    public abstract class Person
+    {
+        public string Name { get; protected set; }
+        public string Surname { get; protected set; }
+        public string PhoneNumber;
+        public Person(string _name, string _surname)
+        {
+            Name = _name;
+            Surname = _surname;
+        }
+    }
+
+    public class Employee : Person
+    {
+        public CompanyStore WorkPlace { get; private set; }
+        public string Position { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public DateTime EmploymentDate { get; private set; }
+
+        public Employee(string _name, string _surname) : this(_name, _surname, "noNumber") {  }
+
+        public Employee(string _name, string _surname, string _phoneNumber) : this(_name, _surname, _phoneNumber, new DateTime(2000,01,01)) { }
+
+        public Employee(string _name, string _surname, string _phoneNumber, DateTime _birthDate)
+            : this(_name, _surname, _phoneNumber, _birthDate, "сотрудник") { }
+
+        public Employee(string _name, string _surname, string _phoneNumber, DateTime _birthDate, string _position) : base(_name, _surname)
+        {
+            PhoneNumber = _phoneNumber;
+            BirthDate = _birthDate;
+            Position = _position;
+            EmploymentDate = DateTime.Now;
+            AllStaff.Add(this);
+        }
+
+        public Employee this[string _searchString]
+        {
+            get
+            {
+                foreach (var person in AllStaff.GetAllStaff())
+                {
+                    if ((person.Name.ToLower() == _searchString.ToLower()) || (person.Surname.ToLower() == _searchString.ToLower()))
+                    {
+                        return person;
+                    }
+                }
+                return null;
+            }
+        }
+        public Employee this[string _name, string _surname]
+        {
+            get
+            {
+                foreach (var person in AllStaff.GetAllStaff())
+                {
+                    if ((person.Name.ToLower() == _name.ToLower()) && (person.Surname.ToLower() == _surname.ToLower()))
+                    {
+                        return person;
+                    }
+                }
+                return null;
+            }
+        }
+        public List<Employee> this[DateTime _birthdate]
+        {
+            get
+            {
+                var result = new List<Employee>();
+                foreach (var person in AllStaff.GetAllStaff())
+                {
+                    if (person.BirthDate == _birthdate)
+                    {
+                        result.Add(person);
+                    }
+                }
+                return result;
+            }
+        }
+    }
+
+    public static class AllStaff
+    {
+        private static readonly List<Employee> Staff;
+        public static uint Count { get; private set; }
+        static AllStaff()
+        {
+            Staff = new();
+        }
+
+        public static void Add(Employee _employee)
+        {
+            Staff.Add(_employee);
+            Count++;
+        }
+        public static List<Employee> GetAllStaff()
+        {
+            return Staff;
+        }
+        public static void PrintAllStaff()
+        {
+            foreach (var person in Staff)
+            {
+                Console.WriteLine($"{person.Name}, {person.Surname},  {person.Position},  {person.PhoneNumber},  {person.BirthDate}");
+            }
+        }
+    }
+
+    public class Customer : Person
+    {
+        public string Rank { get; private set; }
+        public Customer(string _name, string _surname) : base(_name, _surname)
+        {
+            Rank = "Новичок";
+        }
+    }
+}
---
+++ Modul7_Final/Modul7_/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using static ConsoleHelper_50.Helper_50;
+
+namespace BazarMiniMarket
+{
+    internal class Program
+    {
+        /// Вынес эту переменную в глобальную область, чтобы работать с ней в разных методах 
+        /// Представим, что она есть в БД ))
+        public static CompanyStore myStore;
+        
+        static void Main(string[] args)
+        {
+            WriteLn("Hello World!");
+
+            Init();
+            WriteLn("");
+            var Staff = AllStaff.GetAllStaff();
+            WriteLn("Personal data about every Employee (FROM STATIS CLASS 'AllStaff')", ConsoleColor.White);
+            AllStaff.PrintAllStaff();
+
+            WriteLn("");
+            WriteLn("Lets add some staff and print all list again", ConsoleColor.White);
+            ChangeInfo();
+            AllStaff.PrintAllStaff();
+        }
+
+        private static void Init()
+        {
+            Employee Me = new Employee("Aleksey", "Shatalov", "8904", new DateTime(1993, 01, 07));
+            myStore = new CompanyStore()
+            {
+                StoreAddress = new Address()
+                {
+                    Region = "Russia",
+                    City = "Belgorod",
+                    Street = "Esenina",
+                    Number = 1,
+                    AddNumber = 0,
+                    PostCode = 308160,
+                    Floor = 2,
+                    Office = 223,
+                    Building = string.Empty,
+                    Description = "Head office of my amazing Company - \"Bazar Mini Market\""
+                },
+                Director = Me,
+                ShortName = "Head Office",
+                PhoneNumber = Me.PhoneNumber,
+                WorkSchedule = "Пн-Пт с 10 до 15"
+            };
+
+            WriteLn($"And now we have {CompanyStore.StoreCount} stores in out company.", ConsoleColor.Green);
+            WriteLn($"It's address is {myStore.StoreAddress.ShortAddress}, Phone number is /{myStore.PhoneNumber}/");
+            WriteLn($"Boss is: {myStore.Director.Name} {myStore.Director.Surname}, was born at {myStore.Director.BirthDate}. Tel: {myStore.Director.PhoneNumber}",ConsoleColor.White);
+
+            WriteLn("");
+            WriteLn("Lets buy new phone for working purposes");
+            Me.PhoneNumber = "8915";
+            WriteLn($"My new telephone number is /{Me.PhoneNumber}/ (FROM VARIABLE 'ME')");
+            WriteLn($"New telephone number OF FIRST STORE is /{myStore.PhoneNumber}/ (FROM VARIABLE 'myStore', assignment through the boss (PhoneNumber = Director.PhoneNumber))");
+            WriteLn("Not work... =(", ConsoleColor.Red);
+
+            myStore.PhoneNumber = Me.PhoneNumber;
+            WriteLn($"New telephone number OF FIRST STORE is /{myStore.PhoneNumber}/ (FROM VARIABLE 'myStore', direct assignment)");
+            WriteLn("Ok, that is fine", ConsoleColor.Green);
+
+            WriteLn("");
+            WriteLn("Let's work hard, mafaka!!!!", ConsoleColor.Blue);
+        }
+        private static void ChangeInfo()
+        {
+            Employee Zam = new Employee("Elena", "Titova", "8800", new DateTime(1993, 02, 25));
+            Employee anotherBoy = new Employee("Aleksey", "Merkulov", "8921", new DateTime(1975, 10, 17));
+
+            /// В строке ниже получилось, что я через экземпляр класса Employee обратился к его индексатору, 
+            /// который в свою очередь, обратился к статичному полю Staff статичного класса AllStaff, 
+            /// представляющего собой список List<Employee>.  По-моему, это "per rectum".. )))
+            /// В реальной задаче я бы постарался такого не избежать, но тут сделал, чисто ради науки))
+            var boss = Zam["aleksey"];
+
+
+        }
+    }
+}
